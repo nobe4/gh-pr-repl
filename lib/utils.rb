584c1276a694b6733cc2feaf5a985e8cc9b4f39e
nobe4 <nobe4@github.com>
2021-10-21 12:21:39 +0200

    Parse query strings

--- lib/utils.rb
+++ lib/utils.rb
@@ -9,45 +9,91 @@ class Utils
     result = `gh pr view --json 'url' --jq '.url' 2>&1`
 
     unless $?.success? # rubocop:disable Style/SpecialGlobalVars
-      puts "Couldn't find the current repo or branch."
+      puts "! Couldn't find the current repo or branch.".yellow
       exit(1)
     end
 
     result
   end
 
-  def self.parse_current_reference(reference)
+  def self.parse_pr_url(reference)
     # https://github.com/<owner>/<repo>/pull/<number>
-    if %r{https://github.com/([^/]+)/([^/]+)/pull/(\d+)} =~ reference
-      repo = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
-      number = Regexp.last_match(3)
-      branch, base, closed = `gh pr view --json 'headRefName,baseRefName,closed' --jq '"\\(.headRefName),\\(.baseRefName),\\(.closed)"' #{reference}`.strip.split(',')
+    return unless %r{https://github.com/([^/]+)/([^/]+)/pull/(\d+)} =~ reference
 
-      return [repo, number, branch, base, closed] if $?.success? # rubocop:disable Style/SpecialGlobalVars
-    end
+    repo = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
+    number = Regexp.last_match(3)
+    branch, base, closed = `gh pr view --json 'headRefName,baseRefName,closed' --jq '"\\(.headRefName),\\(.baseRefName),\\(.closed)"' #{reference}`.strip.split(',')
 
+    [repo, number, branch, base, closed] if $?.success? # rubocop:disable Style/SpecialGlobalVars
+  end
+
+  def self.parse_owner_repo_branch(reference)
     # <owner>/<repo>/<branch>
-    if %r{([^/]+)/([^/]+)/(.+)} =~ reference
-      repo = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
-      branch = Regexp.last_match(3)
-      number, base, closed = `gh pr view --repo #{repo} --json 'number,baseRefName,closed' --jq '"\\(.number),\\(.baseRefName),\\(.closed)"' #{branch}`.strip.split(',')
+    return unless %r{([^/]+)/([^/]+)/(.+)} =~ reference
 
-      return [repo, number, branch, base, closed] if $?.success? # rubocop:disable Style/SpecialGlobalVars
+    repo = "#{Regexp.last_match(1)}/#{Regexp.last_match(2)}"
+    branch = Regexp.last_match(3)
+    number, base, closed = `gh pr view --repo #{repo} --json 'number,baseRefName,closed' --jq '"\\(.number),\\(.baseRefName),\\(.closed)"' #{branch}`.strip.split(',')
+
+    [repo, number, branch, base, closed] if $?.success? # rubocop:disable Style/SpecialGlobalVars
+  end
+
+  def self.parse_query(query)
+    prs = `gh api -X GET search/issues --paginate -f q='#{query}' -q '.items[] | .html_url'`
+    prs = prs.split("\n")
+
+    prs.map do |pr|
+      parse_pr_url(pr)
     end
+  end
 
+  def self.parse_current_repo(reference)
     # Try to infer the current repo, if we can't do this, we can't continue.
     repo = `gh repo view --json 'owner,name' --jq '"\\(.owner.login)/\\(.name)"'`.strip
-    unless $?.success? # rubocop:disable Style/SpecialGlobalVars
-      puts "× Couldn't find current repo , exiting.".red
-      exit(1)
+    return unless $?.success? # rubocop:disable Style/SpecialGlobalVars
+
+    if repo.nil?
+      puts "! Couldn't find current repo, skipping.".yellow
+      return
     end
 
     # <branch> or <pull_number>
-    # gh can find the right one automatically
+    # gh can find the right one automatically given the repo.
     branch, base, number, closed = `gh pr view --repo #{repo} --json 'headRefName,baseRefName,number,closed' --jq '"\\(.headRefName),\\(.baseRefName),\\(.number),\\(.closed)"' #{reference}`.strip.split(',')
     return [repo, number, branch, base, closed] if $?.success? # rubocop:disable Style/SpecialGlobalVars
 
-    puts "× Couldn't parse '#{reference}', exiting.".red
-    exit(1)
+    puts "× Couldn't parse '#{reference}', skipping.".yellow
+  end
+
+  def self.parse_references(references)
+    parsed_references = []
+
+    references.each do |reference|
+      parsed_reference = parse_pr_url(reference)
+      unless parsed_reference.nil?
+        parsed_references << parsed_reference
+        next
+      end
+
+      parsed_reference = parse_owner_repo_branch(reference)
+      unless parsed_reference.nil?
+        parsed_references << parsed_reference
+        next
+      end
+
+      parsed_reference = parse_query(reference)
+      unless parsed_reference.nil?
+        parsed_references.push(*parsed_reference)
+        next
+      end
+
+      parsed_reference = parse_current_repo(reference)
+      unless parsed_reference.nil?
+        parsed_references << parsed_reference
+        next
+      end
+    end
+
+    parsed_references
   end
 end
